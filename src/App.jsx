88ffa313e2cb7f81470c9dfa752bcb86f1f78f0f
geoivelina <geoivelina@hotmail.com>
2023-11-10 11:27:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import UserList from "./components/UserList";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main className="main">
+                <UserList />
+
+                
+            </main>
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
